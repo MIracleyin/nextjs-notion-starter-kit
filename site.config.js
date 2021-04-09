6195f864a6284433d9d35a673be279d06f9aef68
Miracleyin <miracleyin@live.com>
2021-04-09 19:53:46 +0800

    init

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '00fc2c39e2804e11b3255e91961a6e2d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Yin Zhang',
+  domain: 'yinzhang.it',
+  author: 'YIn Zhang',
 
   // open graph metadata (optional)
   description: 'Example site description',
